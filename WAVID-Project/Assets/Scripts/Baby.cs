9351046249a4127316a6695ec69463a1bf35155b
Tonia B <toniabeglari@gmail.com>
2017-01-21 15:22:35 -0800

    wavid player controller and baby attachment

---
+++ WAVID-Project/Assets/Scripts/Baby.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baby : MonoBehaviour {
+
+	public GameObject babySeat;
+	bool isFound = false;
+	Rigidbody rb;
+	public float thrust = 6f;
+
+	public PlayerController playerControl;
+
+	// Use this for initialization
+	void Start () {
+		playerControl = GameObject.Find ("Player").GetComponent<PlayerController> ();
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFound) {
+			Vector3 relativePos = babySeat.transform.position - transform.position;
+			rb.AddForce(relativePos * thrust);
+			//rb.MovePosition(babySeat.transform.position * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name.Equals ("Player") && !isFound) {
+			isFound = true;
+			playerControl.foundBabies.Add (GetComponent<Rigidbody> ());
+			print ("found");
+		}
+	}
+}
---
+++ WAVID-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public Transform rightProp;
+	public Transform leftProp;
+	public float thrust = 2f;
+	public float turnThrust = .2f;
+	public float torque = 1f;
+
+	public List<Rigidbody> foundBabies = new List<Rigidbody>();
+
+	int minTurnCount = 3;
+	int turnCount = 0;
+	enum Paddle{right, left};
+	Paddle lastPressed = Paddle.left;
+
+	Rigidbody rb;
+	Rigidbody rightRB;
+	Rigidbody leftRB;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rightRB = rightProp.GetComponent<Rigidbody> ();
+		leftRB = leftProp.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float currThrust = thrust;
+		float direction;
+
+		if (Input.GetKeyDown ("l")) { //RIGHT HAND DOWN
+
+			if (lastPressed != Paddle.right && turnCount <= -minTurnCount) { //WERE YOU PREVIOUSLY TURNING LEFT
+				turnCount = 0;
+			} 
+			turnCount++;
+
+			if (turnCount > minTurnCount) {
+				currThrust = turnThrust;
+			}
+
+			MovePlayer (rightProp.up * torque, transform.forward * currThrust);
+			lastPressed = Paddle.right;
+
+		} else if (Input.GetKeyDown ("a")) { //LEFT HAND DOWN
+
+			if (lastPressed != Paddle.left && turnCount >= minTurnCount) {
+				turnCount = 0;
+			} 
+
+			turnCount--;
+			
+			if (turnCount <= -minTurnCount) {
+				currThrust = turnThrust;
+			}
+
+			MovePlayer (leftProp.up * -torque, transform.forward * currThrust);
+			//rb.AddTorque(leftProp.up * -torque);
+			//rb.AddForce (transform.forward * currThrust);
+			lastPressed = Paddle.left;
+		}
+			
+	}
+
+	void MovePlayer(Vector3 torqueVector, Vector3 forceVector) {
+		rb.AddTorque(torqueVector);
+		rb.AddForce (forceVector);
+
+
+		foundBabies.ForEach (delegate(Rigidbody babyRB)
+			{
+				babyRB.AddTorque(torqueVector);
+				babyRB.AddForce (forceVector);
+			});
+				
+	}
+}
