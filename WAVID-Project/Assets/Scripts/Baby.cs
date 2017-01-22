7f2e6835bb02c772e0915ed276f2e823a1db289a
Daniel Batista <Daniel Batista>
2017-01-21 20:48:55 -0800

    adjusting how the babies follow floty Papa

--- WAVID-Project/Assets/Scripts/Baby.cs
+++ WAVID-Project/Assets/Scripts/Baby.cs
@@ -3,22 +3,49 @@ using System.Collections;
 
 public class Baby : MonoBehaviour {
 
-	public GameObject babySeat;
+
 	public bool isFound = false;
+	bool approaching;
 	Rigidbody rb;
 	public float thrust = 6f;
 
+	public PlayerController playerControl;
+
+	public float veryClose = 0.5f;
+	Transform babySeat;
+
 	// Use this for initialization
 	void Start () {
+		playerControl = GameObject.Find ("Player").GetComponent<PlayerController> ();
 		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isFound) {
-			Vector3 relativePos = babySeat.transform.position - transform.position;
+			if (Vector3.Distance (transform.position, babySeat.position) <= veryClose) {
+				if (approaching) {					
+					babySeat = playerControl.babySeats [playerControl.seat_i];
+					playerControl.seat_i++;
+					approaching = false;
+				}
+			}
+			Vector3 relativePos = babySeat.position - transform.position;
 			rb.AddForce(relativePos * thrust);
 			//rb.MovePosition(babySeat.transform.position * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name.Equals ("Player") && !isFound) {
+			isFound = true;
+			approaching = true;
+			playerControl.foundBabies.Add (GetComponent<Rigidbody> ());
+			if (playerControl.seat_i % 2 == 0) {
+				babySeat = playerControl.xtrmSeatL;
+			} else {
+				babySeat = playerControl.xtrmSeatR;
+			}
 		}
 	}
 }
